18780a96a58e20f4b67b0058fbe81cc1711fe96c
Yagamii <yagogomesc@gmail.com>
2017-06-12 19:58:31 -0300

    Ajustado layout responsivo de carrinho

--- src/template/Carrinho/index.php
+++ src/template/Carrinho/index.php
@@ -1,49 +1,58 @@
-<h1 align="center">Carrinho</h1>
-	<br/>
-	<table width="100%">
+<div class="page-header" style="margin-top: -15px">
+  <h1 align="center">Carrinho</h1>
+</div>
+
+
+
+	<table class="table">
 		<tr>
 			<th>Produto</th>
-			<th>Descrição</th>
+			<th class="hidden-xs">Descrição</th>
 			<th>Valor</th>
-			<th>Quantidade</th>
+			<th>Qntd.</th>
 			<th></th>
 		</tr>
-	
+
 	<?php if(!isset($_SESSION['carrinho']) or empty($_SESSION['carrinho'])):?>
-		<tr>
-			<td colspan="5"><p>Seu carrinho está vazio no momento. :(</p></td>
-		</tr>
+		<tbody>
+			<tr>
+				<td align="center" colspan="5">
+					<p>Seu carrinho está vazio no momento. :(</p>
+				</td>
+			</tr>
+		</tbody>
 	</table>
-	<?php 
+	<?php
 		else:
+			echo '<tbody>';
 			foreach($_SESSION['carrinho'] as $id => $qnt):
 				$row = $Carrinho->getProdutoById($id);
 	?>
 		<tr>
-			<td id="carrinho-produtos"><?php echo $row[0]['nome_produto'];?></td>
-			<td id="carrinho-produtos"><?php echo $row[0]['descricao']; ?></td>
-			<td id="carrinho-produtos"<?php echo $row[0]['valor']; ?></td>
-			<td id="carrinho-produtos"><?php echo $qnt;?></td>
-			<td id="carrinho-produtos">
+			<td><?php echo $row[0]['nome_produto'];?></td>
+			<td class="hidden-xs"><?php echo $row[0]['descricao']; ?></td>
+			<td><?php echo $row[0]['valor']; ?></td>
+			<td><?php echo $qnt;?></td>
+			<td>
 				<form action="index.php?page=carrinho" method="POST" name="removerProduto">
 					<input type="hidden" name="id_produto" value="<?php echo $id;?>" />
 					<input type="hidden" name="removeProduto" value="TRUE" />
-					<input type="submit" name="remover" value="Remover" />
+					<input type="submit" class="btn btn-default" name="remover" value="Remover" />
 				</form>
 			</td>
 		</tr>
-	<?php 
+
+	<?php
 		endforeach;
 	?>
+</tbody>
 	</table>
 	<form action="index.php?page=carrinho" method="POST" name="confirmarCompra">
 		<p align="right">
 			<input type="hidden" name="confirma" value="TRUE" />
-			<input type="submit" name="confirmaCompra" value="Finalizar compra" style="padding: 5px 5px; border-radius: 5px 5px 5px 5px"/>
+			<input type="submit" name="confirmaCompra" value="Finalizar compra" class="btn btn-primary"/>
 		</p>
 	</form>
 	<?php
 		endif;
 	?>
-	
-	
