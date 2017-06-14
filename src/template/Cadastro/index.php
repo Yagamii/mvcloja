05117be36dd552b029bcdb388f4c087eb4823d85
Yagamii <yagogomesc@gmail.com>
2017-06-14 19:48:59 -0300

    Ajustado layout

--- src/template/Cadastro/index.php
+++ src/template/Cadastro/index.php
@@ -1,28 +1,55 @@
-<h1 align="center">Cadastro</h1>
+<div class="page-header" style="margin-top: -15px">
+	<h1 align="center">Cadastro</h1>
+</div>
 
-<form name="cadastro" action="index.php?page=cadastro" method="post">
-	<div class="form-cadastro">
-    	<div class="form-label">
-			<span>Nome: </span><br/>
-        	<span>Sobrenome: </span><br/>
-            <span>Usuário: </span><br/>
-            <span>E-mail: </span><br/>
-            <span>Senha: </span><br/>
-            <span>Confirmação: </span>
-        </div>
-        
-        <div class="form-input">
-        	<input type="text" name="nome" value="<?php if(isset($_POST['nome'])) echo $_POST['nome']; ?>"/><br/>
-            <input type="text" name="sobrenome" value="<?php if(isset($_POST['sobrenome'])) echo $_POST['sobrenome']; ?>"/><br/>
-            <input type="text" name="usuario" value="<?php if(isset($_POST['usuario'])) echo $_POST['usuario']; ?>"/><br/>
-            <input type="text" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>"/><br/>
-            <input type="password" name="senha" /><br/>
-            <input type="password" name="confirmsenha" />
-        </div>
-        
-        <div class="botao-cadastro">
-        	<input type="hidden" name="cadastrar" value="TRUE" />
-            <input type="submit" name="cadastro" value="Cadastrar" />
-        </div>
+<form name="cadastro" class="form-horizontal" action="index.php?page=cadastro" method="post">
+	<div class="form-group">
+			<label class="control-label col-sm-2 col-sm-offset-2" for="nome">Nome:</label>
+			<div class="col-sm-4">
+				<input type="text" name="nome" class="form-control" id="nome" value="<?php if(isset($_POST['nome'])) echo $_POST['nome']; ?>" placeholder="Nome"/>
+			</div>
 	</div>
+
+	<div class="form-group">
+		<label class="control-label col-sm-2 col-sm-offset-2" for="sobrenome">Sobrenome:</label>
+		<div class="col-sm-4">
+			<input type="text" name="sobrenome" class="form-control" id="sobrenome" value="<?php if(isset($_POST['sobrenome'])) echo $_POST['sobrenome'];?>" placeholder="Sobrenome"/>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label class="control-label col-sm-2 col-sm-offset-2" for="usuario">Usuário:</label>
+		<div class="col-sm-4">
+			<input type="text" name="usuario" class="form-control" id="usuario" value="<?php if(isset($_POST['usuario'])) echo $_POST['usuario']; ?>" placeholder="Usuário"/>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label class="control-label col-sm-2 col-sm-offset-2" for="email">Email:</label>
+		<div class="col-sm-4">
+			<input type="email" name="email" class="form-control" id="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>" placeholder="Email"/>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label class="control-label col-sm-2 col-sm-offset-2" for="senha">Senha:</label>
+		<div class="col-sm-4">
+			<input type="password" class="form-control" name="senha" placeholder="Senha" />
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label class="control-label col-sm-2 col-sm-offset-2" for="confirmsenha">Confirmação:</label>
+		<div class="col-sm-4">
+			<input type="password" class="form-control" name="confirmsenha" placeholder="Confirmação" />
+		</div>
+	</div>
+
+	<div class="form-group">
+		<div class="col-sm-offset-4 col-sm-4 col-xs-offset-4">
+			<input type="hidden" name="cadastrar" value="TRUE" />
+			<button type="submit" class="btn btn-default" name="cadastro">Cadastrar</button>
+		</div>
+	</div>
+
 </form>
--- src/template/Login/index.php
+++ src/template/Login/index.php
@@ -1,10 +1,27 @@
-<h1 align="center"> Login </h1>
-<form action="index.php?page=login" name="login" method="post">
-	<div class="login-form">
-    	<p>Login: <input type="text" name="user" /></p>
-        <p>Senha: <input type="password" name="pass" /></p>
-        <br/>
-        <input type="hidden" name="logando" value="TRUE" />
-        <input class="botao-enviar" type="submit" name="login" value="Login" />
-    </div>
-</form>
+<div class="page-header" style="margin-top: -15px">
+	<h1 align="center"> Login </h1>
+</div>
+
+	<form action="index.php?page=login" class="form-horizontal" name="login" method="post">
+		<div class="form-group">
+			<label class="control-label col-sm-2 col-sm-offset-3" for="user">Login:</label>
+			<div class="col-sm-3">
+				<input type="text" name="user" class="form-control" />
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label style="margin-top: -7px" class="control-label col-sm-2 col-sm-offset-3" for="pass">Senha:</label>
+			<div class="col-sm-3">
+				<input type="password" name="pass" style="margin-top: -7px" class="form-control" />
+			</div>
+		</div>
+
+		<div class="form-group">
+			<div class="col-sm-offset-5 col-sm-2 col-xs-offset-4">
+				<input type="hidden" name="logando" value="TRUE" />
+				<button class="btn btn-primary" type="submit" name="login"/>Login</button>
+			</div>
+		</div>
+
+	</form>
